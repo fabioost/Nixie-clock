a8b6030f4f02db02d7956a9c5dcb24ed94e63ac4
fabioost <fabioost@bol.com.br>
2016-07-11 21:11:55 -0300

    Adicao da biblioteca RTC e modificacao dos metodos de saida para display

--- Relogio_1/Relogio_1.ino
+++ Relogio_1/Relogio_1.ino
@@ -4,7 +4,10 @@ This code cycles through the digits of a Nixie Tube.
 Autor: Fabio Leandro Ost
 Modificado: 06/06/16
 */
+#include <Wire.h>                       // For some strange reasons, Wire.h must be included here
+#include <DS1307new.h>
 
+uint16_t TimeIsSet = 0xaa55; // Helper that time must not set again
 
 int switch1 = 1;
 int switch2 = 2;
@@ -12,15 +15,26 @@ int switch3 = 4;
 int switch4 = 7;
 int switch5 = 12;
 int switch6 = 13;
-
+int hora=0, minuto=0, segundo=0, dia=0, mes=0, ano=0;
 
 int i=0;
-long interval = 1000; 
+long interval = 1000; //intervalo de atualizacao display 1s
 long previousMillis = 0;
-long intervalPlex = 3; 
-long previousMillisPlex = 0;
+long intervalPlex = 3; //frequencia do multiplex 55hz
 
 void setup() {
+  Wire.begin();//ajustar rtc
+  RTC.getRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
+  if (TimeIsSet != 0xaa55){
+    //ajustaDataHora();
+    RTC.stopClock();   
+    RTC.fillByYMD(2016,7,11);
+    RTC.fillByHMS(20,30,0);
+    RTC.setTime();
+    TimeIsSet = 0xaa55;
+    RTC.setRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
+    RTC.startClock();
+  }
  
   pinMode(11, OUTPUT);// D Pinos de comunicacao 74141N
   pinMode(10, OUTPUT);// C
@@ -36,7 +50,38 @@ void setup() {
 }
  
 void loop() {
-  int digito[7] = {0,0,1,2,3,4,5};
+    
+    
+}
+
+//-------------------------------------------------//
+//METODOS:
+void mostraHora(){
+     unsigned long currentMillis = millis();
+     if(currentMillis - previousMillis > interval) {
+       RTC.getTime();
+       //int dia = RTC.day;
+       //int mes = RTC.month;
+       //int ano = RTC.year;
+       hora = RTC.hour;
+       minuto= RTC.minute;
+       segundo = RTC.second;
+       previousMillis = currentMillis;
+     }
+     paraDisplay(hora, minuto, segundo);
+     
+}
+
+void paraDisplay(int par1, int par2, int par3){//saida para display
+    int digito[7] = {0,0,0,0,0,0,0};
+    
+    digito[1]=int(par1/10);
+    digito[2]=int(par1%10);
+    digito[3]=int(par2/10);
+    digito[4]=int(par2%10);
+    digito[5]=int(par3/10);
+    digito[6]=int(par3%10);
+    
    
     digitalWrite(switch1, HIGH);
     digitalWrite(switch2, LOW);
@@ -91,20 +136,8 @@ void loop() {
     digitalWrite(switch6, HIGH);
     nixie(digito[6]);
     delay(intervalPlex);
-    
   
-    unsigned long currentMillis = millis();
-    if(currentMillis - previousMillis > interval) {
-        i++;
-        previousMillis = currentMillis;  
-    
-    } 
-  
-    
 }
-
-//-------------------------------------------------//
-//METODOS:
 
 void nixie(int num){ //metodo de acionamento de um digito
   
