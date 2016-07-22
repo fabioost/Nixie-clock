55c98d4a95a06354ffce3dd1c31fb414bd47dd59
fabioost <fabioost@bol.com.br>
2016-07-22 19:34:40 -0300

    Adicao de codigos para expansor de portas(PCF8574) e ajuste manual de hora.

--- Relogio_1/Relogio_1.ino
+++ Relogio_1/Relogio_1.ino
@@ -7,6 +7,14 @@ Modificado: 06/06/16
 #include <Wire.h>                       // For some strange reasons, Wire.h must be included here
 #include <DS1307new.h>
 
+#define expanderAddr B0111000
+#define BT_0  0 
+#define BT_1  1
+//...//
+#define BT_5  5
+#define BT_6  6
+#define BT_7  7
+
 uint16_t startAddr = 0x0000;            // Start address to store in the NV-RAM
 uint16_t lastAddr;                      // new address for storing in NV-RAM
 uint16_t TimeIsSet = 0xffff; // Helper that time must not set again
@@ -30,14 +38,16 @@ void setup() {
   RTC.setRAM(0, (uint8_t *)&startAddr, sizeof(uint16_t));// Store startAddr in NV-RAM address 0x08 
   RTC.getRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
   if (TimeIsSet != 0xaa55){
-    //ajustaDataHora();
+    ajustaHora();
+    /*
     RTC.stopClock();   
     RTC.fillByYMD(2016,7,11);
     RTC.fillByHMS(18,30,0);
     RTC.setTime();
+    */
     TimeIsSet = 0xaa55;
     RTC.setRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
-    RTC.startClock();
+    //RTC.startClock();
   }
  
   pinMode(11, OUTPUT);// D Pinos de comunicacao 74141N
@@ -69,7 +79,7 @@ void mostraHora(){
        segundo = RTC.second;
        previousMillis = currentMillis;
      }
-     //Serial.print(hora);
+     //Serial.print(hora);//debugin
      //Serial.print(minuto);
      //Serial.println(segundo);
      
@@ -225,4 +235,103 @@ void nixie(int num){ //metodo de acionamento de um digito
       break;
   }
   
+}
+byte expanderRead() {
+  byte _data;
+  Wire.requestFrom(expanderAddr, 1);
+  if(Wire.available()) {
+    _data = Wire.read();
+  }
+  return _data;
+}
+
+void ajustaHora(){
+  int botao=0,hora=-1,minuto=-1,dia=1,mes=8,ano=2016;
+  
+  while(botao!=3 || hora==-1){
+     botao=0;
+     
+     byte returnByte = expanderRead();
+     boolean bitStatus = bitRead(returnByte, BT_5);
+     botao+=1*bitStatus;
+     returnByte = expanderRead();
+     bitStatus = bitRead(returnByte, BT_6);
+     botao+=2*bitStatus;
+     returnByte = expanderRead();
+     bitStatus = bitRead(returnByte, BT_7);
+     botao+=3*bitStatus;
+     //delay(100);
+     if(botao==1){
+       hora++;
+       if(hora>24) hora=1;
+     }else{
+       if(botao==2){
+         hora--;
+         if(hora<1) hora=24;
+       }
+     }
+     telaAjusteHora(hora);
+     
+   }
+   botao=0;
+   while(botao!=3 || minuto==-1){
+     botao=0;
+     
+     byte returnByte = expanderRead();
+     boolean bitStatus = bitRead(returnByte, BT_5);
+     botao+=1*bitStatus;
+     returnByte = expanderRead();
+     bitStatus = bitRead(returnByte, BT_6);
+     botao+=2*bitStatus;
+     returnByte = expanderRead();
+     bitStatus = bitRead(returnByte, BT_7);
+     botao+=3*bitStatus;
+     //delay(100);
+     if(botao==1){
+       minuto++;
+       if(minuto>59) minuto=0;
+     }else{
+       if(botao==2){
+         minuto--;
+         if(minuto<0) minuto=59;
+       }
+     }
+     telaAjusteMinuto(minuto);
+     
+   }
+   
+    RTC.stopClock();   
+    RTC.fillByYMD(ano,mes,dia);
+    RTC.fillByHMS(hora,minuto,0);
+    RTC.setTime();
+    RTC.startClock(); 
+}
+
+void telaAjusteHora( int hora){  
+ if(hora==-1){
+   hora=0;
+   piscaTela();
+ }else{
+  paraDisplay(hora, 0, 0);
+  } 
+}
+
+void telaAjusteMinuto( int minuto){
+  if(minuto==-1) minuto=0;
+  paraDisplay(hora, minuto, 0); 
+}
+
+void piscaTela(){
+    digitalWrite(switch1, LOW);
+    digitalWrite(switch2, LOW);
+    digitalWrite(switch3, LOW);
+    digitalWrite(switch4, LOW);
+    digitalWrite(switch5, LOW);
+    digitalWrite(switch6, LOW);
+    delay(500);
+    unsigned long currentMillis = millis();
+    while(millis() < currentMillis + 500){
+      paraDisplay(0, 0, 0);
+    }
+    
 }
